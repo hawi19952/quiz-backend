7ad526a39bed590348bf007fd1f2c5768912f027
hawi19952 <hussain@mohtwize.com>
2023-03-24 14:13:48 +0300

    setup environment

---
+++ src/helpers/env.ts
@@ -0,0 +1,15 @@
+import { config } from 'dotenv';
+
+const { NODE_ENV } = process.env;
+if(!NODE_ENV) {
+  console.log(`please define NODE_ENV in the environment to decide the .env file needed`);
+  process.exit(0);
+}
+
+config({
+  path: `./env/${NODE_ENV}.env`
+});
+
+export const {
+  PORT
+} = process.env;
