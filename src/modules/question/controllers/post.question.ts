7afcca709b6e53bd7d90662c35905a208df0a809
hawi19952 <hussain@mohtwize.com>
2023-04-02 00:54:40 +0300

    fix post question attributes

--- src/modules/question/controllers/post.question.ts
+++ src/modules/question/controllers/post.question.ts
@@ -1,8 +1,19 @@
 import { Request, Response } from "express";
 import createQuestion from "../functions/create.question.js";
+import reportError from "../../../helpers/report.error.js";
+import sendResponse from "../../../helpers/send.response.js";
 
 export default async (req: Request, res: Response) => {
-  const { quizId, question } = req.body;
+  const { quizId, statement, isMandatory, answers } = req.body;
+  const question = {
+    statement,
+    isMandatory,
+    answers
+  }
   const createdQuestion = await createQuestion(quizId, question);
-  return createdQuestion;
+  
+  if(!createdQuestion) {
+    return reportError("Couldn't create question", 500, res);
+  }
+  return sendResponse(createdQuestion, res);
 }
