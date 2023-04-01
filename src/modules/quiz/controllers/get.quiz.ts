680cf7fa0534b9c11d2f5a6b0f2ebfefde2423a1
hawi19952 <hussain@mohtwize.com>
2023-04-01 22:45:43 +0300

    clean up

--- src/modules/quiz/controllers/get.quiz.ts
+++ src/modules/quiz/controllers/get.quiz.ts
@@ -1,24 +1,12 @@
 import { Request, Response } from "express";
 import reportError from "../../../helpers/report.error.js";
-import { Answer, Question, Quiz, sequelize } from "../../../helpers/database/models.js";
 import sendResponse from "../../../helpers/send.response.js";
+import getQuizById from "../functions/get.quiz.by.id.js";
 
 export default async (req: Request, res:Response) => {
   const { id } = req.params; 
 
-  const foundQuiz = await Quiz.findOne(
-    {
-      where: {
-        id
-      },
-      include: {
-        model: Question,
-        include: [{
-          model: Answer
-        }]
-      }
-    }
-  )
+  const foundQuiz = await getQuizById(id);
 
   if(!foundQuiz) {
     return reportError(`No Quiz Found with the id : ${id}`, 2, res)
---
+++ src/modules/quiz/functions/get.quiz.by.id.ts
@@ -0,0 +1,18 @@
+import { Quiz, Question, Answer } from "../../../helpers/database/models.js"
+
+export default async (quizId: number | string) => {
+  const foundQuiz = await Quiz.findOne(
+    {
+      where: {
+        quizId
+      },
+      include: {
+        model: Question,
+        include: [{
+          model: Answer
+        }]
+      }
+    }
+  )
+  return foundQuiz
+}
