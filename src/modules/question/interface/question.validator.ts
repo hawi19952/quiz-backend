fbe4691da456e187922ffb5cc66b07f9f629dee0
hawi19952 <hussain@mohtwize.com>
2023-04-02 00:53:55 +0300

    add validation to put operation

--- src/modules/question/interface/question.validator.ts
+++ src/modules/question/interface/question.validator.ts
@@ -2,7 +2,7 @@ import Joi from "joi";
 import { answerValidation } from "../../quiz/interface/quiz.validator.js";
 
 export default Joi.object().keys({
-  quizId: Joi.string().min(1).required(),
+  quizId: Joi.number().min(1).required(),
   statement: Joi.string().required(),
   isMandatory: Joi.boolean().default(false),
   answers: Joi.array().items(answerValidation)
@@ -11,3 +11,8 @@ export default Joi.object().keys({
 export const questionIdValidation = Joi.object().keys({
   id: Joi.number().min(1).required()
 });
+
+export const questionUpdateValidator = Joi.object().keys({
+  statement: Joi.string().required(),
+  isMandatory: Joi.boolean().default(false),
+})
--- src/modules/question/question.routes.ts
+++ src/modules/question/question.routes.ts
@@ -2,7 +2,7 @@ import { Application } from "express";
 import controllers from "./controllers/index.js";
 import bodyParser from "../../helpers/body.parser.js";
 import { validateBody, validateParams } from "../../helpers/validate.request.js";
-import questionValidator, { questionIdValidation } from "./interface/question.validator.js";
+import questionValidator, { questionIdValidation, questionUpdateValidator } from "./interface/question.validator.js";
 import { quizIdValidation } from "../quiz/interface/quiz.validator.js";
 
 
@@ -11,5 +11,5 @@ const route = '/api/question';
 export default (app: Application) => {
   app.get(`${route}/:id` ,validateParams(questionIdValidation) ,controllers.getQuestion);
   app.post(`${route}`, bodyParser, validateBody(questionValidator) ,controllers.postQuestion);
-  app.put(`${route}/:id`, bodyParser, validateParams(quizIdValidation) ,validateBody(questionValidator), controllers.putQuestion);
+  app.put(`${route}/:id`, bodyParser, validateParams(quizIdValidation) ,validateBody(questionUpdateValidator), controllers.putQuestion);
 }
