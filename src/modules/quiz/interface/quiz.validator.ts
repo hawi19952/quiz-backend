fa36a6b0cfc75b9608a96bab2f151fa07769c681
hawi19952 <hussain@mohtwize.com>
2023-04-01 16:48:24 +0300

    create sample quiz + breakdown validation

--- src/modules/quiz/interface/quiz.validator.ts
+++ src/modules/quiz/interface/quiz.validator.ts
@@ -1,19 +1,21 @@
 import Joi from "joi";
 
+const answerValidation = Joi.object().keys({
+  statement: Joi.string(),
+  isCorrect: Joi.boolean().default(false)
+})
+
+
+const questionValidation = Joi.object().keys({
+  statement: Joi.string().required(),
+  isMandatory: Joi.boolean().default(false),
+  answers: Joi.array().items(answerValidation)
+})
+
+
 export default Joi.object().keys({
   title: Joi.string().min(1).required(),
   description: Joi.string(),
   isPublished: Joi.boolean().default(false),
-  questions: Joi.array().items(
-    Joi.object().keys({
-      statement: Joi.string().required(),
-      isMandatory: Joi.boolean().default(false),
-      answers: Joi.array().items(
-        Joi.object().keys({
-          statement: Joi.string(),
-          isCorrect: Joi.boolean().default(false)
-        })
-      )
-    })
-  )
+  questions: Joi.array().items(questionValidation)
 })
